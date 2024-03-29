3751d4fe7c1c004fea1cd756c2d1ef8c39e10e09
nicoleinae <nicoleinae@gmail.com>
2024-03-29 20:33:36 -0300

    Fist class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumb() {
+		return column;
+	}
+
+	public void setColumb(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
